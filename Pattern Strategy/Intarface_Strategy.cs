86a7a509770dbe546432f6deb7758e0bcc0e5721
Turma ADS IFRO <ads_ifro_2022@outlook.com>
2023-04-12 21:53:21 -0400

    Adicionar arquivos de projeto.

---
+++ Pattern Strategy/Intarface_Strategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Strategy
+{
+    public abstract class Intarface_Strategy
+    {
+        public string Street;
+
+    }
+}
---
+++ Pattern Strategy/StrategyClass/Bicicleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Strategy.StrategyClass
+{
+    public class Bicicleta : Intarface_Strategy
+    {
+        public Bicicleta()
+        {
+            Street = "Faixa de Pedestre";
+        }
+    }
+}
